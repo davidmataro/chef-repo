01ccf487766ac7261eb2bb6661f933af92842160
David Mataró <david@webbambu.com>
2014-07-04 10:46:44 +0200

    my cookbook

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,12 @@
+name             'my_cookbook'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'jo@davidmataro.com'
+license          ''
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends				'build-ssential'
+depends				'apache2', '>= 1.0.4'
+depends				'chef-client'
+depends				'apt'
+depends				'ntp'
---
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: my_cookbook
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+include_recipe	"chef-client"
+include_recipe	"apt"
+include_recipe	"ntp"
